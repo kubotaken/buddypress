fba9d91ca4d1cd8d84d37829a265fc253278c2a4
kenken <wesley6x6x6@gmail.com>
2012-01-29 23:00:14 +0900

    Update bp-themes/bp-default/activity/entry.php

--- bp-themes/bp-default/activity/entry.php
+++ bp-themes/bp-default/activity/entry.php
@@ -80,7 +80,7 @@
 				<?php do_action( 'bp_activity_entry_meta' ); ?>
   
 
-<a href="<?php bp_activity_thread_permalink() ?>" title="view" name="view" value="view">View</a>
+	<a href="<?php bp_activity_thread_permalink() ?>" title="view">View</a>
 		</div>
 
 		<?php endif; ?>
@@ -88,10 +88,10 @@
 	</div>
 	<div class="sharebutton">
 		<span class="fb">
-<iframe src="//www.facebook.com/plugins/like.php?href=<?php bp_activity_thread_permalink() ?>&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21"scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe></span>
+	<iframe src="//www.facebook.com/plugins/like.php?href=<?php bp_activity_thread_permalink() ?>&amp;send=false&amp;layout=button_count&amp;width=100&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21"scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe></span>
 
 		<span class="tw">
-<a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
+	<a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></span>
 
 	</div>
